e6d921a2435c6f1a71dd88f06d8aaeb5aafdd8c9
arip <ariprusli105@gmail.com>
2020-12-15 17:55:54 +0700

    implementasi flex message

--- public/index.php
+++ public/index.php
@@ -1,96 +1,116 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
- 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
- 
-use \LINE\LINEBot;
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
-use \LINE\LINEBot\SignatureValidator as SignatureValidator;
- 
-$pass_signature = true;
- 
-// set LINE channel_access_token and channel_secret
-$channel_access_token = "oR7KbOGXhjWKgTVPPXoGYvpQr2TZ4BDE/4ITC3DP+Tbng0Zy+JqyQDPkVuH822885sdYkH9yWgLFbyjWCQA1ji3Uq4vwGzFqb58uzmwt1497YJ6FRgxIGS/Mhq71n+Ca1CbodIVCHFzvqzJbu82BUwdB04t89/1O/w1cDnyilFU=";
-$channel_secret = "90b6be0d747c6300ee878514d2062b62";
- 
-// inisiasi objek bot
-$httpClient = new CurlHTTPClient($channel_access_token);
-$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
- 
-$app = AppFactory::create();
-$app->setBasePath("/public");
- 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello World!");
-    return $response;
-});
- 
-// buat route untuk webhook
-$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
-    // get request body and line signature header
-    $body = $request->getBody();
-    $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
- 
-    // log body and signature
-    file_put_contents('php://stderr', 'Body: ' . $body);
- 
-    if ($pass_signature === false) {
-        // is LINE_SIGNATURE exists in request header?
-        if (empty($signature)) {
-            return $response->withStatus(400, 'Signature not set');
-        }
- 
-        // is this request comes from LINE?
-        if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
-            return $response->withStatus(400, 'Invalid signature');
-        }
-    }
- 
+$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature){
+
+    require __DIR__ . '/../vendor/autoload.php';
     
-// kode aplikasi 
-
-$data = json_decode($body, true);
-    if (is_array($data['events'])) {
-        foreach ($data['events'] as $event) {
-            if ($event['type'] == 'message')
-            {
-                if ($event['message']['type'] == 'text')
-                {
-                    //send message as reply to user
-                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
-
-
-                    $response->getBody()->write(json_encode($result->getJSONDecodeBody()));
-                    return $response
-                        ->withHeader('Content-Type', 'application/json')
-                        ->withStatus($result->getHTTPStatus());
-                }
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Slim\Factory\AppFactory;
+    
+    use \LINE\LINEBot;
+    use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+    use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+    use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+    
+    $pass_signature = true;
+    
+    // set LINE channel_access_token and channel_secret
+    $channel_access_token = "oR7KbOGXhjWKgTVPPXoGYvpQr2TZ4BDE/4ITC3DP+Tbng0Zy+JqyQDPkVuH822885sdYkH9yWgLFbyjWCQA1ji3Uq4vwGzFqb58uzmwt1497YJ6FRgxIGS/Mhq71n+Ca1CbodIVCHFzvqzJbu82BUwdB04t89/1O/w1cDnyilFU=";
+    $channel_secret = "90b6be0d747c6300ee878514d2062b62";
+    
+    // inisiasi objek bot
+    $httpClient = new CurlHTTPClient($channel_access_token);
+    $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+    
+    $app = AppFactory::create();
+    $app->setBasePath("/public");
+    
+    $app->get('/', function (Request $request, Response $response, $args) {
+        $response->getBody()->write("Hello World!");
+        return $response;
+    });
+    
+    // buat route untuk webhook
+    $app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+        // get request body and line signature header
+        $body = $request->getBody();
+        $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
+    
+        // log body and signature
+        file_put_contents('php://stderr', 'Body: ' . $body);
+    
+        if ($pass_signature === false) {
+            // is LINE_SIGNATURE exists in request header?
+            if (empty($signature)) {
+                return $response->withStatus(400, 'Signature not set');
+            }
+    
+            // is this request comes from LINE?
+            if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
+                return $response->withStatus(400, 'Invalid signature');
             }
         }
-        return $response->withStatus(200, 'for Webhook!'); //untuk ngasih respon 200 saat verify webhook
-    }
-    return $response->withStatus(400, 'No event sent!');
-});
+    
+        
+    // kode aplikasi 
 
-$app->get('/pushmessage', function ($req, $response) use ($bot) {
-    // send push message to user
-    $userId = 'U1132cb95ebc8fcf91f45bd9430d991f5';
-    $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan push');
-    $result = $bot->pushMessage($userId, $textMessageBuilder);
- 
-    $response->getBody()->write("Pesan push berhasil dikirim!");
-    return $response
-        ->withHeader('Content-Type', 'application/json')
-        ->withStatus($result->getHTTPStatus());
-});
+    $data = json_decode($body, true);
+        if (is_array($data['events'])) {
+            foreach ($data['events'] as $event) {
+                if ($event['type'] == 'message')
+                {
+                    if ($event['message']['type'] == 'text'){
+                    if (strtolower($event['message']['text']) == 'user id') {
+                        
 
+                        $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
+
+                        } elseif (strtolower($event['message']['text']) == 'felx message'){
+                            $flexTemplate = file_get_contents("../flex_message.json"); //template flex message
+                            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                                'replyToken' => $event['replyToken'],
+                                'message' => [
+                                    [
+                                        'type'     => 'flex',
+                                        'altText'  => 'Test Flex Message',
+                                        'contents' => json_decode($flexTemplate)
+                                    ]
+                                ],
+                            ]);
+
+                        }else{
+                        //send message as reply to user
+                        $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+
+                        $response->getBody()->write(json_encode($result->getJSONDecodeBody()));
+                        return $response
+                            ->withHeader('Content-Type', 'application/json')
+                            ->withStatus($result->getHTTPStatus());
+                    }
+                }
+            }
+            return $response->withStatus(200, 'for Webhook!'); //untuk ngasih respon 200 saat verify webhook
+        }
+        return $response->withStatus(400, 'No event sent!');
+    });
+
+    $app->get('/pushmessage', function ($req, $response) use ($bot) {
+        // send push message to user
+        $userId = 'U1132cb95ebc8fcf91f45bd9430d991f5';
+        $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan push');
+        $result = $bot->pushMessage($userId, $textMessageBuilder);
+    
+        $response->getBody()->write("Pesan push berhasil dikirim!");
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus($result->getHTTPStatus());
+    });
+}
 
 $app->run();
